e82ba057271e531fbcd10748486e698f45009e73
Luiz Fernando <38297606+lfnd0@users.noreply.github.com>
2018-10-13 13:27:10 -0300

    Atualização
    Danillo me enviou esta versão pelo Whatsapp.

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # Jogo-da-Cobrinha---Projeto-de-P1-LP1
-<h3>Linguagens e Tecnologias usadas:</h3>
-<ul>
-  <li>Python</li>
-  <li>IDE Pygame</li>
-</ul>
-  
--- cobrinha.py
+++ cobrinha.py
@@ -1,51 +1,44 @@
+import os
+import sys, traceback
 import pygame, random
 from pygame.locals import *
 
-def cria_campo_esquerdo():
-    contaCampo = 30
-    while(1==1):
-        if(contaCampo <= 420):
-            campo_esquerdo.append((10,contaCampo))
-        else:
-            break
-        contaCampo += 10
-
-def cria_campo_direito():
-    contaCampo = 30
-    while(1==1):
-        if(contaCampo <= 420):
-            campo_direito.append((480,contaCampo))
-        else:
-            break
-        contaCampo += 10
-
-def cria_campo_abaixo():
-    contaCampo = 10
-    while(1==1):
-        if(contaCampo <= 480):
-            campo_direito.append((contaCampo,430))
-        else:
-            break
-        contaCampo += 10
-
-def cria_campo_acima():
-    contaCampo = 10
-    while(1==1):
-        if(contaCampo <= 480):
-            campo_direito.append((contaCampo,30))
-        else:
-            break
-        contaCampo += 10
-
 def cria_campo():
-    cria_campo_esquerdo()
-    cria_campo_direito()
-    cria_campo_abaixo()
-    cria_campo_acima()
+    contaCampo = 30
+    while(1==1):
+        if(contaCampo <= 420):
+            campo.append((10,contaCampo))
+        else:
+            break
+        contaCampo += 10
+
+    contaCampo = 30
+    while(1==1):
+        if(contaCampo <= 420):
+            campo.append((480,contaCampo))
+        else:
+            break
+        contaCampo += 10
+
+    contaCampo = 10
+    while(1==1):
+        if(contaCampo <= 480):
+            campo.append((contaCampo,430))
+        else:
+            break
+        contaCampo += 10
+
+    contaCampo = 10
+    while(1==1):
+        if(contaCampo <= 480):
+            campo.append((contaCampo,30))
+        else:
+            break
+        contaCampo += 10
 
 def cria_comida():
-    x = random.randint(30,470)
-    y = random.randint(30,420)
+    x = random.randint(20,470)
+    y = random.randint(40,420)
     return (x//10 * 10, y//10 * 10)
 
 def colisao(c1, c2):
@@ -56,26 +49,18 @@ DIREITA = 1
 PRABAIXO = 2
 ESQUERDA = 3
 
+pontuacao = 0
+
 pygame.init()
+pygame.mixer.init()
+pygame.font.init()
 
 tela = pygame.display.set_mode((500,450))
 pygame.display.set_caption('COBRINHA')
 
-campo_direito = []
-campoTotal_direito = pygame.Surface((10,10))
-campoTotal_direito.fill((255,255,255))
-
-campo_esquerdo = []
-campoTotal_esquerdo = pygame.Surface((10,10))
-campoTotal_esquerdo.fill((255,255,255))
-
-campo_abaixo = []
-campoTotal_abaixo = pygame.Surface((10,10))
-campoTotal_abaixo.fill((255,255,255))
-
-campo_acima = []
-campoTotal_acima = pygame.Surface((10,10))
-campoTotal_acima.fill((255,255,255))
+campo = []
+campoTotal = pygame.Surface((10,10))
+campoTotal.fill((255,255,255))
 
 cobrinha = [(200, 200), (210, 200), (220,200)]
 cobrinhaCresce = pygame.Surface((10,10))
@@ -85,17 +70,23 @@ posicaoComida = cria_comida()
 comida = pygame.Surface((10,10))
 comida.fill((0,0,255))
 
-texto = pygame.font.get_default_font()
-textoPerdeu = pygame.font.SysFont(texto, 50)
+#textoPerdeu = pygame.font.Font ("fontes / Minecraft.ttf" ,  26)
 
-pygame.font.init()
+texto1 = pygame.font.SysFont("Minecraft", 30)
+textoPerdeu = texto1.render("VOCE PERDEU!", True, (0, 128, 0))
+texto2 = pygame.font.SysFont("Minecraft", 17)
+texto_pontuacao = texto2.render("pontuacao: "+str(pontuacao), True, (0, 128, 0))
+
+pygame.mixer.music.load("sons/somFundo.mp3")
+pygame.mixer.music.play()
 
 vou_para = ESQUERDA
 
-clock = pygame.time.Clock()
+tempo = pygame.time.Clock()
 
+andar = True
 while True:
-    clock.tick(10)
+    tempo.tick(10)
     for evento in pygame.event.get():
         if evento.type == QUIT:
             pygame.quit()
@@ -110,33 +101,49 @@ while True:
             if evento.key == K_RIGHT:
                 vou_para = DIREITA
 
-    if colisao(cobrinha[0], posicaoComida):
-        posicaoComida = cria_comida()
-        cobrinha.append((0,0))
+    if andar:
 
-    for i in range(len(cobrinha) - 1, 0, -1):
-        cobrinha[i] = (cobrinha[i-1][0], cobrinha[i-1][1])
+        if colisao(cobrinha[0], posicaoComida):
+            posicaoComida = cria_comida()
+            cobrinha.append((0,0))
+            pontuacao += 10
+            texto_pontuacao = texto2.render("pontuacao: "+str(pontuacao), True, (0, 128, 0))
 
-    if vou_para == PRACIMA:
-        cobrinha[0] = (cobrinha[0][0], cobrinha[0][1] - 10)
-    if vou_para == PRABAIXO:
-        cobrinha[0] = (cobrinha[0][0], cobrinha[0][1] + 10)
-    if vou_para == DIREITA:
-        cobrinha[0] = (cobrinha[0][0] + 10, cobrinha[0][1])
-    if vou_para == ESQUERDA:
-        cobrinha[0] = (cobrinha[0][0] - 10, cobrinha[0][1])
+            pygame.mixer.music.load("sons/comer.mp3")
+            pygame.mixer.music.play()
+            pygame.display.update()
+            pygame.time.delay(500)
+            pygame.mixer.music.load("sons/somFundo.mp3")
+            pygame.mixer.music.play()
+
+        for i in range(len(cobrinha) - 1, 0, -1):
+            cobrinha[i] = (cobrinha[i-1][0], cobrinha[i-1][1])
+
+        if vou_para == PRACIMA:
+            cobrinha[0] = (cobrinha[0][0], cobrinha[0][1] - 10)
+        if vou_para == PRABAIXO:
+            cobrinha[0] = (cobrinha[0][0], cobrinha[0][1] + 10)
+        if vou_para == DIREITA:
+            cobrinha[0] = (cobrinha[0][0] + 10, cobrinha[0][1])
+        if vou_para == ESQUERDA:
+            cobrinha[0] = (cobrinha[0][0] - 10, cobrinha[0][1])
 
     cria_campo()
 
     tela.fill((0,0,0))
-    for posicaoCampo in campo_esquerdo:
-        tela.blit(campoTotal_esquerdo,posicaoCampo)
-    for posicaoCampo in campo_direito:
-        tela.blit(campoTotal_direito,posicaoCampo)
-    #text = textoPerdeu.render('Texto Perdeu deu certo!', 1, (255,255,255))
+    tela.blit(texto_pontuacao, (200, 10))
+    for posicaoCampo in campo:
+        tela.blit(campoTotal,posicaoCampo)
     tela.blit(comida, posicaoComida)
-    #tela.blit(text, (150, 150))
-    for posicaoCobri in cobrinha:
-        tela.blit(cobrinhaCresce,posicaoCobri)
+    for posicaoCobrinha in cobrinha:
+        tela.blit(cobrinhaCresce,posicaoCobrinha)
+    if((cobrinha[0][0] <= 10) or (cobrinha[0][0] >= 480) or (cobrinha[1][1] <= 30) or (cobrinha[1][1] >= 430)):
+        #pygame.mixer.music.load("sons/fim.mp3")
+        #pygame.mixer.music.play()
+        #del cobrinha[0]
+        pygame.mixer.music.stop()
+        andar = False
+        tela.blit(textoPerdeu, (120, 150))
+        #som de derrota e retornar para o menu
 
     pygame.display.update()
